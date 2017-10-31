98850900fb79121953437ffa9c492a3980a79585
dhruvang1 <dhruvangmakadia1@gmail.com>
2017-11-01 00:52:01 +0530

    Added reduce points

--- Version_1/src/ScienceExtraction.java
+++ Version_1/src/ScienceExtraction.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class ScienceExtraction {
     private int threshold = 100;
+    private double epsilon = 2;
 
     public void extract(String srcImgPath, String destImgPath) throws IOException{
         Mat imgSrc = Imgcodecs.imread(srcImgPath, Imgcodecs.IMREAD_COLOR);
@@ -23,18 +24,80 @@ public class ScienceExtraction {
         Mat cannyOutput = new Mat();
         List<MatOfPoint> contours = new ArrayList<>();
         Mat heirarchy = new Mat();
-        Imgproc.Canny(imgGray,cannyOutput,threshold,threshold*2,3,false);
+        Imgproc.Canny(imgGray,cannyOutput,threshold,threshold*2,3,true);
+//        Imgcodecs.imwrite("D:\\MTP\\TacGen\\canny.jpg",cannyOutput);
 
-        Imgproc.findContours(cannyOutput,contours,heirarchy,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE, new Point(0,0));
+        int erosion_size = 3;
+        int dilation_size = 3;
+        Mat elementErosion = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erosion_size , erosion_size));
+        Mat elementDilation = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilation_size, dilation_size));
+
+        Imgproc.dilate(cannyOutput,cannyOutput,elementDilation);
+//        Imgcodecs.imwrite("C:\\Users\\Dhruvang\\Desktop\\dilate.jpg",cannyOutput);
+
+        Imgproc.erode(cannyOutput,cannyOutput,elementErosion);
+//        Imgcodecs.imwrite("C:\\Users\\Dhruvang\\Desktop\\erode.jpg",cannyOutput);
+
+
+        Imgproc.findContours(cannyOutput,contours,heirarchy,Imgproc.RETR_CCOMP,Imgproc.CHAIN_APPROX_TC89_KCOS, new Point(0,0));
         BufferedWriter writer = new BufferedWriter(new FileWriter(destImgPath+"\\"+"regions.txt",false));
+        int originalPoints = 0;
+        int reducedPoints = 0;
         for(int i=0;i<contours.size();i++){
             List <Point> tempContour = contours.get(i).toList();
+            originalPoints += tempContour.size();
+            tempContour = reducePoints(tempContour,0,tempContour.size()-1);
+            reducedPoints += tempContour.size();
             int tempSize = tempContour.size();
             for(int j=0;j<tempSize-1;j++){
                 writer.append(String.format("%d,%d,",(int)tempContour.get(j).x,(int)tempContour.get(j).y));
             }
             writer.append(String.format("%d,%d\n",(int)tempContour.get(tempSize-1).x,(int)tempContour.get(tempSize-1).y));
         }
+        System.out.println("Original points: " + originalPoints);
+        System.out.println("Reduced points: " + reducedPoints);
         writer.close();
+    }
+
+    //Douglas–Peucker algorithm
+    public List<Point> reducePoints(List<Point> contour, int start, int end) {
+        double dmax = 0;
+        int index = 0;
+        for(int i=start+1;i<=end-1;i++){
+            double distance = pointLineDistance(contour.get(start),contour.get(end),contour.get(i));
+//            if(distance == 0){
+//                System.out.println("zero distance " + contour.get(start) + contour.get(end) + contour.get(i));
+//            }
+            if(distance > dmax){
+                dmax = distance;
+                index = i;
+            }
+        }
+
+        //change it to % of length
+        List<Point> pointList = new ArrayList<>();
+        if(dmax > epsilon){
+            List<Point> pointList1 = reducePoints(contour,start,index);
+            List<Point> pointList2 = reducePoints(contour,index,end);
+            pointList1.remove(pointList1.size()-1); // index element will repeat
+            pointList.addAll(pointList1);
+            pointList.addAll(pointList2);
+        } else {
+            pointList.add(contour.get(start));
+            pointList.add(contour.get(end));
+        }
+        return pointList;
+    }
+
+    public double pointLineDistance(Point start, Point end, Point point){
+        if(start == end){
+            return Math.sqrt((start.x - point.x)*(start.x - point.x) + (start.y - point.y)*(start.y - point.y));
+        }
+
+        double a = (start.y - end.y);
+        double b = (end.x - start.x);
+        double c = start.x*end.y - start.y*end.x;
+
+        return Math.abs(a*point.x + b*point.y + c) / Math.sqrt(a*a+b*b);
     }
 }
